be389365fd0a604e0cec16be73a05dd398cae89c
RoninsWork <130744166+verygood-agency@users.noreply.github.com>
2023-07-18 14:21:17 +0300

    Добавил новые скрипты и показ балуна с числом товаров по итогу фильтрации

---
+++ js/new.js
@@ -0,0 +1,27 @@
+// Получаем все элементы, которые мы будем отслеживать
+const filterParams = document.querySelectorAll('.filter-param');
+
+filterParams.forEach((param) => {
+  param.addEventListener('input', function (event) {
+    // Удаляем старые блоки уведомления
+    const oldNotification = document.querySelector('.notification');
+    if (oldNotification) {
+      oldNotification.remove();
+    }
+
+    // Создаем новую кнопку уведомления
+    const notification = document.createElement('button');
+    notification.className = 'notification button';
+    notification.type = 'submit';
+    notification.textContent = 'Показать 1145ewrwer6'; // замените это на свое значение
+
+    // Добавляем событие клика
+    notification.addEventListener('click', function() {
+      // Здесь добавьте ваш код для фильтрации товаров
+      console.log('Фильтрация товаров...');
+    });
+
+    // Добавляем кнопку уведомления после измененного параметра фильтра
+    this.parentNode.appendChild(notification);
+  });
+});
